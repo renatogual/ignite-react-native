eeab7ae99977fed3520b9a19cd5fc3cc5c2ea95c
renatogual <renatogual@hotmail.com>
2022-04-12 07:20:14 -0300

    feat: finalized list transactions

--- gofinances/src/screens/Dashboard/index.tsx
+++ gofinances/src/screens/Dashboard/index.tsx
@@ -9,11 +9,49 @@ import {
   UserName,
   Icon,
   HighlightCards,
+  Transactions,
+  Title,
+  TransactionList,
 } from './styles'
 
 import { HighlightCard } from '../../components/HighlightCard'
+import {
+  TransactionCard,
+  TransactionCardProps,
+} from '../../components/TransactionCard'
+
+export interface DataListProps extends TransactionCardProps {
+  id: string
+}
 
 export function Dashboard() {
+  const data: DataListProps[] = [
+    {
+      id: '1',
+      type: 'positive',
+      title: 'Desenvolvimento de site',
+      amount: 'R$ 12.000,00',
+      category: { name: 'Vendas', icon: 'dollar-sign' },
+      date: '13/04/2020',
+    },
+    {
+      id: '2',
+      type: 'negative',
+      title: 'Restaurante',
+      amount: 'R$ 59,00',
+      category: { name: 'Alimentação', icon: 'coffee' },
+      date: '13/04/2020',
+    },
+    {
+      id: '3',
+      type: 'negative',
+      title: 'Aluguel do apartamento',
+      amount: 'R$ 1.200,00',
+      category: { name: 'Casa', icon: 'shopping-bag' },
+      date: '13/04/2020',
+    },
+  ]
+
   return (
     <Container>
       <Header>
@@ -36,10 +74,35 @@ export function Dashboard() {
       </Header>
 
       <HighlightCards>
-        <HighlightCard />
-        <HighlightCard />
-        <HighlightCard />
+        <HighlightCard
+          title='Entradas'
+          amount='R$ 17.400,00'
+          lastTransaction='Última entrada dia 13 de abril'
+          type='up'
+        />
+        <HighlightCard
+          title='Saídas'
+          amount='R$ 1.259,00'
+          lastTransaction='Última entrada dia 03 de abril'
+          type='down'
+        />
+        <HighlightCard
+          title='Entradas'
+          amount='R$ 16.141,00'
+          lastTransaction='01 à 16 de abril'
+          type='total'
+        />
       </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+
+        <TransactionList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />
+      </Transactions>
     </Container>
   )
 }
