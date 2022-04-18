9d0efcb26e90bc486e0c7d01bc8449a8eee9da75
renatogual <renatogual@hotmail.com>
2022-04-18 19:52:01 -0300

    fix: add config to use intl lib

--- gofinances/App.tsx
+++ gofinances/App.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import AppLoading from 'expo-app-loading'
 
+import 'intl'
+import 'intl/locale-data/jsonp/pt-BR'
+
 import {
   useFonts,
   Poppins_400Regular,
