25519e953284b2e39baaca8fd7763c89febde454
renatogual <renatogual@hotmail.com>
2022-05-24 09:01:20 -0300

    fix warning

--- rentx/src/routes/app.tab.routes.tsx
+++ rentx/src/routes/app.tab.routes.tsx
@@ -31,7 +31,7 @@ export function AppTabRoutes() {
       }}
     >
       <Screen
-        name="Home"
+        name="StackRoutes"
         component={AppStackRoutes}
         options={{
           tabBarIcon: ({ color }) => (
